6d7866d894f3249b65e69917cd9366f77efdf94f
rinsuki <428rinsuki+git@gmail.com>
2025-04-27 00:33:38 +0900

    Redisの型エラーを修正

--- src/backend/redis.ts
+++ src/backend/redis.ts
@@ -1,5 +1,5 @@
-import { default as Redis } from "ioredis"
+import { default as Redis, type Redis as RedisType } from "ioredis"
 
 const REDIS_URL = process.env.REDIS_URL ?? "redis://localhost:6379"
 
-export const redis = new Redis(REDIS_URL)
+export const redis: RedisType = new Redis(REDIS_URL)
